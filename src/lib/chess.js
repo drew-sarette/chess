f5bf60492ec49630a6eab04c129b380a8ca50627
drew-sarette <103331340+drew-sarette@users.noreply.github.com>
2024-11-06 16:45:18 -0600

    Add basic moves for each piece
    Each piece has a direction and a depth it is allowed to move.  The Board.ray method plots all candidate moves on the board. The normalMoves function processes those to remove friendly fire and returns an array of coordinates the piece can move to.

--- src/lib/chess.js
+++ src/lib/chess.js
@@ -1,86 +1,132 @@
-const board = [
-  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"], // 0
-  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"], // 1
-  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"], // 2
-  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"], // 3
-  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"], // 4
-  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"], // 5
-  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"], // 6
-  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"], // 7
-  //  0     1     2     3     4     5     6     7
-];
-const startPosition = [
-  ["R", "N", "B", "Q", "K", "B", "N", "R"],
-  ["P", "P", "P", "P", "P", "P", "P", "P"],
-  Array(8).fill(null),
-  Array(8).fill(null),
-  Array(8).fill(null),
-  Array(8).fill(null),
-  ["p", "p", "p", "p", "p", "p", "p", "p"],
-  ["r", "n", "b", "q", "k", "b", "n", "r"],
-];
+class Board {
+  static #notationBoard = [
+    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"], // 0
+    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"], // 1
+    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"], // 2
+    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"], // 3
+    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"], // 4
+    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"], // 5
+    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"], // 6
+    ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"], // 7
+    //  0     1     2     3     4     5     6     7
+  ];
+  static #coords = [
+    [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]], // 1
+    [[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]], // 2
+    [[2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7]], // 3
+    [[3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7]], // 4
+    [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7]], // 5
+    [[5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7]], // 6
+    [[6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7]], // 7
+    [[7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7]], // 8
+    // a        b       c       d       e       f       g       h
+  ];
 
-function setBoard(position) {
-  const result = board.slice();
-  for (let rank = 0; rank <= 7; rank++) {
-    for (let file = 0; file <= 7; file++) {
-      const notation = position[rank][file];
-      if (notation) {
-        const color = notation.toUpperCase() === notation ? "white" : "black";
-        switch (notation.toUpperCase()) {
-          case "P":
-            result[rank][file] = new Pawn(color);
-            break;
-          case "R":
-            result[rank][file] = new Rook(color);
-            break;
-          case "B":
-            result[rank][file] = new Bishop(color);
-            break;
-          case "N":
-            result[rank][file] = new Knight(color);
-            break;
-          case "Q":
-            result[rank][file] = new Queen(color);
-            break;
-          case "K":
-            result[rank][file] = new King(color);
-            break;
-        }
-      } else {
-        result[rank][file] = null;
-      }
-    }
+  static #files = [
+    [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]], // a
+    [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1]], // b
+    [[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2]], // c
+    [[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3]], // d
+    [[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4]], // e
+    [[0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5]], // f
+    [[0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6]], // g
+    [[0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7]], // h
+    // 1        2       3       4       5       6       7       8
+  ];
+
+  static get coords() {
+    return this.#coords.slice();
   }
-  return result;
-}
 
-// Return an array of valid squares to move to
-function validTargets(piece, coord, position) {
-  //get all vectors from piece
-  const vectors = piece.vectors(coord);
-  //trim each vector to the next piece, excluding same color and including opposite color (captures)
-  const validVectors = vectors.map((v) => {
-    const result = [];
-    for (let i = 0; i < v.length; i++) {
-      const [rank, file] = v[i];
-      const contents = position[rank][file];
-      if (contents === null) {
-        result.push(v[i]);
-        continue;
-      }
-      if (contents.color === piece.color) {
-        break;
-      }
-      result.push(v[i]);
-      if (contents.color !== piece.color) {
-        break;
+  static #startPosition = [
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    Array(8).fill(null),
+    Array(8).fill(null),
+    Array(8).fill(null),
+    Array(8).fill(null),
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+  ];
+
+  static setBoard(position = Board.#startPosition) {
+    const result = this.coords;
+    for (let rank = 0; rank <= 7; rank++) {
+      for (let file = 0; file <= 7; file++) {
+        const notation = position[rank][file];
+        if (notation) {
+          const color = notation.toUpperCase() === notation ? "white" : "black";
+          switch (notation.toUpperCase()) {
+            case "P":
+              result[rank][file] = new Pawn(color);
+              break;
+            case "R":
+              result[rank][file] = new Rook(color);
+              break;
+            case "B":
+              result[rank][file] = new Bishop(color);
+              break;
+            case "N":
+              result[rank][file] = new Knight(color);
+              break;
+            case "Q":
+              result[rank][file] = new Queen(color);
+              break;
+            case "K":
+              result[rank][file] = new King(color);
+              break;
+          }
+        } else {
+          result[rank][file] = null;
+        }
       }
     }
     return result;
-  });
-  //return a flattened array of those squares.
-  return validVectors.flat();
+  }
+
+  // Takes a starting square, a direction, and a desired depth, and returns all squares on the board in that direction.
+  static ray( origin, direction, depth) {
+    let [rank, file] = origin;
+    const [r, f] = direction;
+    const ray = [];
+
+    rank += r;
+    file += f;
+
+    while (rank >= 0 && rank <= 7 && file >= 0 && file <= 7) {
+      ray.push([rank, file]);
+      rank += r;
+      file += f;
+    }
+    return ray.slice(0, depth);
+  }
+}
+
+
+// Return an array of valid squares to move to
+function normalMoves(piece, coord, position) {
+  //get all directions from piece
+  const {directions, depth} = piece.directions;
+
+  const rays = directions.map(dir => Board.ray(coord, dir, depth));
+  // follow each ray until meeting a piece. Include opposite-colored pieces, exclude same-colored
+  const trimmedRays = rays.map(ray => {
+    for (let i = 0; i < ray.length; i++){
+      const contents = position[ray[i][0]][ray[i][1]];
+      if (!contents) { continue; }
+      if (contents.color === piece.color) {
+        return ray.slice(0, i);
+      }
+      else {
+        return ray.slice(0, i + 1);
+      }
+    }
+    return ray.slice();
+  })
+
+  const normalMoves = trimmedRays.flat();
+  
+  return normalMoves;
 }
 
 class Chess {
@@ -88,7 +134,7 @@ class Chess {
   #history;
   viewing;
   constructor() {
-    this.#history = [setBoard(startPosition)];
+    this.#history = [Board.setBoard()];
   }
 
   get position() {
@@ -110,7 +156,7 @@ class Chess {
   move(selected, target) {
     const piece = this.position[selected[0]][selected[1]];
     if (
-      validTargets(piece, selected, this.position).some((coord) => {
+      normalMoves(piece, selected, this.position).some((coord) => {
         return coord[0] === target[0] && coord[1] === target[1];
       })
     ) {
@@ -175,24 +221,10 @@ class Pawn extends Piece {
     this.short = color === "white" ? "P" : "p";
   }
 
-  vectors([rank, file]) {
+  get directions() {
     const direction = this.color === "white" ? 1 : -1;
-    const vectors = [];
-    if (!this.hasMoved) {
-      vectors.push([
-        [rank + direction, file],
-        [rank + direction + direction, file],
-      ]);
-    } else {
-      vectors.push([[rank + direction, file]]);
-    }
-    if (file < 7) {
-      vectors.push([[rank + direction, file + 1]]);
-    }
-    if (file > 0) {
-      vectors.push([[rank + direction, file - 1]]);
-    }
-    return vectors;
+    const depth = this.hasMoved ? 1 : 2;
+    return { directions: [[direction, 0]], depth: depth }
   }
 }
 
@@ -202,12 +234,9 @@ class Rook extends Piece {
     this.name = "rook";
     this.short = color === "white" ? "R" : "r";
   }
-  vectors([rank, file]) {
-    const direction = this.color === "white" ? 1 : -1;
-    const vectors = [];
-    const horizontal
-    vectors.push([0])
-    return vectors;
+
+  get directions() {
+    return { directions: [[0, 1], [1, 0], [0, -1], [-1, 0]], depth: undefined }
   }
 }
 
@@ -217,6 +246,10 @@ class Knight extends Piece {
     this.name = "knight";
     this.short = color === "white" ? "N" : "n";
   }
+
+  get directions() {
+    return { directions: [[2, 1], [2, -1], [-2, -1], [-2, 1]], depth: 1 }
+  }
 }
 
 class Bishop extends Piece {
@@ -224,6 +257,10 @@ class Bishop extends Piece {
     super(color);
     this.name = "bishop";
     this.short = color === "white" ? "B" : "b";
+  }
+
+  get directions() {
+    return { directions: [[1, 1], [1, -1], [-1, -1], [-1, 1]], depth: undefined }
   }
 }
 
@@ -233,6 +270,9 @@ class Queen extends Piece {
     this.name = "queen";
     this.short = color === "white" ? "Q" : "q";
   }
+  get directions() {
+    return { directions: [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]], depth: undefined }
+  }
 }
 
 class King extends Piece {
@@ -240,6 +280,9 @@ class King extends Piece {
     super(color);
     this.name = "king";
     this.short = color === "white" ? "K" : "k";
+  }
+  get directions() {
+    return { directions: [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]], depth: 1 }
   }
 }
 
